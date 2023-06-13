c48072751d333876835ea4ca4df12bd890d1236b
Raul <you@example.com>
2023-06-13 10:07:17 +0200

    add typeorm repositories to query database & espacio service test

--- src/espacio/espacio.service.spec.ts
+++ src/espacio/espacio.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EspacioService } from './espacio.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Espacio } from './entities/espacio.entity';
+import { CreateEspacioDto } from './dtos/create-espacio.dto';
+
+const espacios: any = [
+  {
+    id: 1,
+    edificio: 'A',
+    aula: '2C',
+  },
+];
 
 describe('EspacioService', () => {
   let service: EspacioService;
-
+  const mockEspacioRepositoryService = {
+    find: jest.fn().mockReturnValue(Promise.resolve(espacios)),
+    save: jest.fn().mockImplementation((createEspacioDto: CreateEspacioDto) => {
+      return {
+        id: 1,
+        ...CreateEspacioDto,
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EspacioService],
+      providers: [
+        EspacioService,
+        {
+          provide: getRepositoryToken(Espacio),
+          useValue: mockEspacioRepositoryService,
+        },
+      ],
     }).compile();
 
     service = module.get<EspacioService>(EspacioService);
@@ -14,5 +39,17 @@ describe('EspacioService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('findAll() should return the array "espacios"', async () => {
+    expect(await service.findAll()).toMatchObject(espacios);
+  });
+  it('should create an space and return the new space "{id: 2,edificio: "B",aula: "A"}" ', async () => {
+    const newSpace = {
+      edificio: 'B',
+      aula: 'A',
+    };
+    expect(await service.create(newSpace)).toMatchObject({
+      id: expect.any(Number),
+    });
   });
 });
--- src/espacio/espacio.service.ts
+++ src/espacio/espacio.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEspacioDto } from './dtos/create-espacio.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Espacio } from './entities/espacio.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EspacioService {
-  async findAll(): Promise<any> {
-    return 'soy espacio service';
+  constructor(
+    @InjectRepository(Espacio) private espacioRepository: Repository<Espacio>,
+  ) {}
+  async findAll(): Promise<Espacio[]> {
+    return this.espacioRepository.find();
+    // return 'soy espacio service';
   }
 
-  create(createEspacioDto: CreateEspacioDto) {
-    return 'post del espacio';
+  async create(createEspacioDto: CreateEspacioDto) {
+    return this.espacioRepository.save(createEspacioDto);
+    // return 'post del espacio';
   }
 }
